3d5c831d9962e79dd972b608e2401a00e0a61e69
ag1224 <arpaniitb.gupta2015@gmail.com>
2017-09-11 18:30:29 +0110

    UP01 completed

---
+++ pintos_UP01/src/userprog/build/userprog/syscall.d
@@ -0,0 +1,9 @@
+userprog/syscall.o: ../../userprog/syscall.c ../../userprog/syscall.h \
+  ../../lib/stdio.h ../../lib/debug.h ../../lib/stdarg.h \
+  ../../lib/stdbool.h ../../lib/stddef.h ../../lib/stdint.h \
+  ../../lib/kernel/stdio.h ../../lib/syscall-nr.h ../../devices/input.h \
+  ../../filesys/file.h ../../filesys/off_t.h ../../filesys/filesys.h \
+  ../../threads/interrupt.h ../../threads/malloc.h ../../threads/synch.h \
+  ../../lib/kernel/list.h ../../threads/thread.h ../../threads/vaddr.h \
+  ../../threads/loader.h ../../userprog/pagedir.h \
+  ../../userprog/process.h
